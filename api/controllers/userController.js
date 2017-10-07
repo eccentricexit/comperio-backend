bf6e1afe72886eb06ef895ace0064766890ece29
Matheus Faria de Alencar <mtsalenc@gmail.com>
2017-10-07 16:53:35 -0300

    fix: Filter result returned

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -8,19 +8,29 @@ exports.create_a_user = function(req, res) {
   var new_user = new User(req.body);
   new_user.save(function(err, user) {
     if (err){res.send(err);}
-    delete user.password;
-    res.json(user);
+
+    var filteredUser = {};
+    filteredUser._id = user._id;
+    filteredUser.username = user.username;
+    filteredUser.name = user.name;
+    filteredUser.latitude = user.latitude;
+    filteredUser.longitude = user.longitude;
+
+    res.json(filteredUser);
   });
 };
 
 
 exports.read_a_user = function(req, res) {
-  User.findById(req.params.userId, function(err, user) {
+  User.findById(req.params.id, function(err, user) {
     if (err){res.send(err);}
+
     var filteredUser = {};
     filteredUser._id = user._id;
     filteredUser.username = user.username;
     filteredUser.name = user.name;
+    filteredUser.latitude = user.latitude;
+    filteredUser.longitude = user.longitude;
 
     res.json(filteredUser);
   });
@@ -28,17 +38,24 @@ exports.read_a_user = function(req, res) {
 
 
 exports.update_a_user = function(req, res) {
-  User.findOneAndUpdate({_id: req.params.userId}, req.body, {new: true}, function(err, user) {
+  User.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, user) {
     if (err){res.send(err);}
-    delete user.password;
-    res.json({message: "User updated.",user});
+
+    var filteredUser = {};
+    filteredUser._id = user._id;
+    filteredUser.username = user.username;
+    filteredUser.name = user.name;
+    filteredUser.latitude = user.latitude;
+    filteredUser.longitude = user.longitude;
+
+    res.json({message: "User updated.",filteredUser});
   });
 };
 
 
 exports.delete_a_user = function(req, res) {
   User.remove({
-    _id: req.params.userId
+    _id: req.params.id
   }, function(err, user) {
     if (err){res.send(err);}
     res.json({ message: 'User successfully deleted' });
