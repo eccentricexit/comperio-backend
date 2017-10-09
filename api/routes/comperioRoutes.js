6710f14881311c593f0547ca514604a0ad93b148
Matheus Faria de Alencar <mtsalenc@gmail.com>
2017-10-08 21:46:18 -0300

    fix: Remove student collection code
    Student information won't be stored on the database anymore.

--- api/routes/comperioRoutes.js
+++ api/routes/comperioRoutes.js
@@ -1,18 +1,6 @@
 'use strict';
 module.exports = function(app) {
-  var users = require('../controllers/userController');
   var schedules = require('../controllers/scheduleController');
-
-  // user Routes
-  app.route('/v1/users')
-    .post(users.create_a_user);
-
-
-  app.route('/v1/users/:id')
-    .get(users.read_a_user)
-    .put(users.update_a_user)
-    .delete(users.delete_a_user);
-
 
   // schedule Routes
   app.route('/v1/schedules')
